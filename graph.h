f8c3174a8fd40e197705d5beb157e050976b8e8f
Mahmoud Zayad <mzayad13@gmail.com>
2023-02-18 14:58:47 -0600

    Updated graph.h, created Vertex and Graph Structs for adjacency Matrix.

--- graph.h
+++ graph.h
@@ -2,14 +2,34 @@
 #define GRAPH
 
 #include <SDL2/SDL.h>
+#include <vector>
+#include <tuple>
 
 using namespace std;
 
-struct Node {
-    bool visited;
+// Data Types
+
+/*
+* Graph (represented by Adjacency matrix)
+* Start - start of path, selected by user.
+* End - end of path, selected by user.
+*/ 
+struct Graph {
+    Vertex start;
+    Vertex end;
+    vector<vector<Vertex>> adjMatrix;
 };
 
-struct Edge {
+/*
+* Vertex
+* visited - if vertex has been visited before.
+* wall - if user made vertex a wall, walls cannot be traversed.
+* cost - cost difference to arrive to this vertex (total edge cost = |v2.cost - v1.cost|)
+*/
+struct Vertex {
+    bool visited = false;
+    bool wall = false; // Can be updated by user
+    int cost = 1; // Default cost, can be adjusted by user
 };
 
 #endif
