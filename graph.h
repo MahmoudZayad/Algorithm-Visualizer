301aceef2323bd728e357bad7226fe3f0a2800cd
Mahmoud Zayad <mzayad13@gmail.com>
2023-02-18 21:52:56 -0600

    Getting seg faults, have to use valgrind

--- graph.h
+++ graph.h
@@ -4,21 +4,14 @@
 #include <SDL2/SDL.h>
 #include <vector>
 #include <tuple>
+#include <iostream>
 
 using namespace std;
 
-// Data Types
-
-/*
-* Graph (represented by Adjacency matrix)
-* Start - start of path, selected by user.
-* End - end of path, selected by user.
-*/ 
-struct Graph {
-    Vertex start;
-    Vertex end;
-    vector<vector<Vertex>> adjMatrix;
-};
+// Dark theme.
+    SDL_Color bG = {22, 22, 22, 255}; // Background - Barely Black
+    SDL_Color gL = {44, 44, 44, 255}; // Grid Lines  - Dark grey
+    SDL_Color cellColor = {89, 152, 214, 255}; // light blue
 
 /*
 * Vertex
@@ -26,10 +19,36 @@ struct Graph {
 * wall - if user made vertex a wall, walls cannot be traversed.
 * cost - cost difference to arrive to this vertex (total edge cost = |v2.cost - v1.cost|)
 */
-struct Vertex {
+struct Cell {
     bool visited = false;
     bool wall = false; // Can be updated by user
     int cost = 1; // Default cost, can be adjusted by user
+
+    tuple<int, int> coord; // maybe unnecessary
+
+    // SDL Rectangle object - color
+    SDL_Color cellFill;
 };
+
+/*
+* Intialize Rectangle with color, size and coord
+*
+*/
+
+void intRect(SDL_Renderer *renderer, SDL_Rect &r, tuple<int,int,int> sizexy) {
+    r.h = get<0>(sizexy);
+    r.w = get<0>(sizexy);
+    r.x = get<1>(sizexy); // Must setup proper spacing with coords
+    r.y = get<2>(sizexy);
+
+    cout << "intRect function" << endl;
+    SDL_SetRenderDrawColor(renderer, 255, gL.g, gL.b, gL.a); // default color off black
+    cout << "intRect function" << endl;
+    SDL_RenderDrawRect(renderer, &r);
+    cout << "intRect function" << endl;
+
+    SDL_RenderPresent(renderer);
+
+}
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,11 +1,32 @@
 #include <SDL2/SDL.h>
 #include <iostream>
 #include <vector>
+#include "graph.h"
 
 using namespace std;
 
-const int WIDTH = 640;
+const int WIDTH = 640; // Temporary until I figure out what to do with resizing
 const int HEIGHT = 480;
+
+const int numVertices = 300; // How many cells we want for now? Maybe ill allow user to adjust
+
+int rectSize = sqrt((WIDTH*HEIGHT)/numVertices); 
+
+// Determine size of grid - used in for loops and intializing gridArray
+int gridHeight = HEIGHT/rectSize;
+int gridWidth = WIDTH/rectSize;
+
+
+vector<vector<tuple<Cell,SDL_Rect>>> grid; // should I use vectors? Yes...
+
+void gridPushback (int x, int y) {
+    vector<tuple<Cell,SDL_Rect>> col;
+    tuple<Cell, SDL_Rect> cell;
+    get<0>(cell).coord = {x, y};
+    col.push_back(cell);
+    grid.push_back(col);
+}
+
 
 int main() {
 
@@ -16,6 +37,24 @@ int main() {
     SDL_Window* window = nullptr;
     SDL_Renderer* renderer = nullptr;
     SDL_CreateWindowAndRenderer(WIDTH, HEIGHT, 0, &window, &renderer);
+
+    SDL_SetRenderDrawColor(renderer, bG.r, bG.g, bG.b, bG.a);
+    SDL_RenderClear(renderer); // clear screen black
+
+    for (int i = 0; i < gridHeight; i++) {
+        for (int j = 0; j < gridWidth; j++) {
+            gridPushback(i,j);
+            intRect(renderer, get<1>(grid[i][j]), {rectSize,i,j}); // intializes cells
+            cout << "grid insert" << i << " " << j << endl; 
+        }
+    }
+    cout << "finished" << endl; 
+    // typeid(grid[1,2]).name();
+    // SDL_Delay(5000); // So it does not close immediately
+    cout << "HELP";
+    // SDL_RenderPresent(renderer);
+    // SDL_Delay(5000); // So it does not close immediately
+
 
     return 0;
 }
